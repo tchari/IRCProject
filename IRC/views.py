a253c14cb75bc892216a827b2ebe83847d0d8329
tchari <tchari@githubiscool.com>
2017-06-22 20:30:02 -0600

    Added new assessment page and corresponding links

--- IRC/views.py
+++ IRC/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def index(request):
 	context = {}
 	return render(request, 'IRC/index.html', context)
+	
+def newAssessment(request):
+	if request.user.is_authenticated:
+		#assessmentList = Assessment.objects.filter(assessor=request.user.id)
+		context = {}
+	else:
+		context = {}
+
+	return render(request, 'IRC/newAssessment.html', context)
