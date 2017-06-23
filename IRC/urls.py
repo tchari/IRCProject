3261ca558019b076d4309828f15b38435979eefb
Tarun Chari <tchari@ACDM.DS.SAIT.CA>
2017-06-22 19:16:46 -0600

    made changes to settings.py and IRC/urls.py

---
+++ IRC/urls.py
@@ -0,0 +1,9 @@
+#IRC APP URL Configuration
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	#url(r'^newAssessment/', views.newAssessment, name='newAssessment')
+]
--- IRCProject/urls.py
+++ IRCProject/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^admin/', admin.site.urls),
+	url(r'^IRC/', include('IRC.urls')),
+	url(r'^', include('django.contrib.auth.urls')),
 ]
